c9cd6dffdcdcafbc8917a4a79a8fbce52f2f2328
Emre Gürses <106022391+emregurs3s@users.noreply.github.com>
2024-09-16 11:52:49 +0300

    her linke uygun hale getirilldi.Update_date tablodan çıkartıldı.Bazı değerler için name eklendi.

--- src/components/DataFetcher.js
+++ src/components/DataFetcher.js
@@ -1,31 +1,55 @@
-import React,{useEffect,useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+
+const DataFetcher = ({ data }) => {
+  if (!data || Object.keys(data).length === 0) {
+    return <div>Veri mevcut değil</div>;
+  }
+
+  const isArray = Array.isArray(data);
+  const updateDate = data["Update_Date"];
+  delete data["Update_Date"];
+
+  const allKeys = Object.keys(data);
+  const exampleValue = data[allKeys[0]] || {}; 
+  const valueHeaders = Object.keys(exampleValue);
+  if (allKeys.some(key => data[key].hasOwnProperty("name"))) {
+    valueHeaders.unshift("name");
+  }
+
+  
+  const cleanedData = allKeys.map(key => {
+    const item = data[key];
+    const cleanedItem = { ...item };
 
 
-const DataFetcher=({data})=>{
-    const keys= Object.keys(data);
-    const exampleValue = data[keys[1]] ; 
-  const valueHeaders = Object.keys(exampleValue); 
+    Object.keys(cleanedItem).forEach(subKey => {
+      if (typeof cleanedItem[subKey] === 'object' && cleanedItem[subKey] !== null) {
+        cleanedItem[subKey] = JSON.stringify(cleanedItem[subKey]);
+      }
+    });
+
+    return { key, ...cleanedItem };
+  });
 
   return (
     <div>
+      {updateDate && <h2>Güncelleme Tarihi: {updateDate}</h2>}
       <table border="1" cellPadding="5" cellSpacing="0">
         <thead>
           <tr>
-        <th> </th>
+            <th>Key</th>
             {valueHeaders.map((header, index) => (
               <th key={index}>{header}</th>
             ))}
-            
           </tr>
         </thead>
         <tbody>
-          {keys.map((key, colIndex) => (
-            <tr key={colIndex}>
-              <td>{key}</td>
-              {valueHeaders.map((header, rowIndex) => (
-                <td key={rowIndex}>
-                  {data[key][header]} 
+          {cleanedData.map((item, rowIndex) => (
+            <tr key={rowIndex}>
+              <td>{item.key}</td>
+              {valueHeaders.map((header, colIndex) => (
+                <td key={colIndex}>
+                  {header === "name" ? item["name"] || '' : item[header] || ''}
                 </td>
               ))}
             </tr>
